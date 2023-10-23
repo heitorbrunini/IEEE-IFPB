34c1b2ed9a37ecba28d928e79fb2b49384517ca3
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2023-10-22 23:54:23 -0300

    import fix

--- src/Components/Paginas/Eventos.js
+++ src/Components/Paginas/Eventos.js
@@ -3,10 +3,10 @@ import Navbar from "../NavBar";
 import ComponenteEvento from "../ComponenteEvento";
 import CardEvento from "../CardEvento";  
 import CardCursoEvento from "../CardCursoEvento";
-import imgCard from "../Assets/upsb_IEEE 1.png" 
-import imgCurso1 from "../Assets/La inteligencia Artificial (IA) en la moda »【Blogdemoda_es】 1.png"
-import imgCurso2 from "../Assets/Photo by alerkiv on Unsplash 1.png" 
-import imgCurso3 from "../Assets/python-decubra-quais-sao-as-suas-funcoes-na-programacao 1.png"
+import imgCard from "../../Assets/upsb_IEEE 1.png" 
+import imgCurso1 from "../../Assets/La inteligencia Artificial (IA) en la moda »【Blogdemoda_es】 1.png"
+import imgCurso2 from "../../Assets/Photo by alerkiv on Unsplash 1.png" 
+import imgCurso3 from "../../Assets/python-decubra-quais-sao-as-suas-funcoes-na-programacao 1.png"
 
 
 export default function Eventos(){ 
